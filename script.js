0495b2ccc26a42e44a0798ef140af7cbd53a4c85
Yash Sharma <139811734+YashSharma412@users.noreply.github.com>
2023-09-29 18:58:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-//your JS code here. If required.
-let ul = document.getElementByTagName('ul')[0];
-let displayText;
+// //your JS code here. If required.
+// let ul = document.getElementByTagName('ul')[0];
+// let displayText;
 
-for(let i=0; i<ul.children.length; i++){
-	let node = ul.children[i];
-	if(node.id == 'level'){
-		displayText = `the level of the element is: ${i+1}`;
-	}
-}
+// for(let i=0; i<ul.children.length; i++){
+// 	let node = ul.children[i];
+// 	if(node.id == 'level'){
+// 		displayText = `the level of the element is: ${i+1}`;
+// 	}
+// }
 
-alert(displayText);
+// alert(displayText);
 
-// const testElement = document.getElementById("level");
-// let levelCount = 1;
-//  while(true){
-// 	 if (levelElement.tagName == "HTML" ) {
-// 	 	break;
-// 	 } 
-// 	 testElement = testElement.parentNode;
-// 	 levelCount++;
-//  }
+let testElement = document.getElementById("level");
+let levelCount = 1;
+ while(true){
+	 if (levelElement.tagName === "HTML" ) {
+	 	break;
+	 } 
+	 testElement = testElement.parentNode;
+	 levelCount++;
+ }
 
-// alert(`The level of the element is: ${levelCount}`);
+alert(`The level of the element is: ${levelCount}`);
